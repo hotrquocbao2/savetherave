c0df7194f35714d37e0c6e51b5855d33cb1d3184
jiji lewandowski <jiji.lewandowski@hmmh.de>
2026-08-13 13:45:03 +0200

    update imprint

--- app/pages/impressum.vue
+++ app/pages/impressum.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    
+  <div class="prose mx-auto">
+    <h1>Impressum</h1>
+    <p>
+      <strong>Verantwortlich im Sinne des Presserechts:</strong><br />
+      <strong>Tokonoma Arts&Culture e.V.</strong><br>
+        Gutleutstr. 320<br>
+        60327 Frankfurt<br />
+      <a href="mailto:kontakt@savetherave.live">kontakt@savetherave.live</a>
+    </p>
   </div>
 </template>
 
